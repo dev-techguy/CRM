91118cf6fc10ef250707596d45efdf9d367680d3
dev-techguy <dev.techguy@gmail.com>
2020-06-29 12:17:36 +0300

    :sparkles: Introducing new features.

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -8,13 +8,6 @@ use Livewire\Component;
 
 class Dashboard extends Component
 {
-    public $lava;
-
-    public function mount()
-    {
-
-    }
-
     public function render()
     {
         return view('livewire.dashboard', [
